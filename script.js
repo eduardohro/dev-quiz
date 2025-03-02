1a2daf36d289424928572b7a027c8ad82a2254e2
Eduardo Rodrigues Oliveira <159492869+eduardohro@users.noreply.github.com>
2025-03-02 18:19:25 -0300

    Add fuction showResultBox

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const main = document.querySelector('.main');
 const continueBtn = document.querySelector('.continue-btn');
 const quizSection = document.querySelector('.quiz-section');
 const quizBox = document.querySelector('.quiz-box');
+const resultBox = document.querySelector('.result-box');
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
@@ -20,7 +21,7 @@ continueBtn.onclick = () => {
     quizSection.classList.add('active');
     popupInfo.classList.remove('active');
     main.classList.remove('active');
-    quizBox.classList.remove('active');
+    quizBox.classList.add('active');
 
     showQuestions(0);
     questionCounter(1);
@@ -44,7 +45,7 @@ nextBtn.onclick = () => {
         nextBtn.classList.remove('active');
 
     } else {
-        console.log('Pergunta concluída')
+        showResultBox();
     }
 }
 
@@ -104,4 +105,29 @@ function questionCounter(index) {
 function headerScore() {
     const headerScoreText = document.querySelector('.header-score');
     headerScoreText.textContent = `Pontuação: ${userScore} / ${questions.length}`;
+}
+
+function showResultBox() {
+    quizBox.classList.remove('active');
+    resultBox.classList.add('active');
+
+    const scoreText = document.querySelector('.score-text');
+    scoreText.textContent = `Sua Pontuação ${userScore} de ${questions.length}`;
+
+    const circularProgress = document.querySelector('.circular-progress');
+    const progressValue = document.querySelector('.progress-value');
+    let progressStartValue = -1;
+    let progressEndValue = (userScore / questions.length) * 100;
+    let speed = 20;
+
+    let progress = setInterval(() => {
+        progressStartValue++;
+
+        progressValue.textContent = `${progressStartValue}%`
+        circularProgress.style.background = `conic-gradient(#c40094 ${progressStartValue * 3.6}deg, rgba(255, 255, 255, .1) 0deg)`;
+
+        if (progressStartValue == progressEndValue) {
+            clearInterval(progress);
+        }
+    }, speed);
 }
