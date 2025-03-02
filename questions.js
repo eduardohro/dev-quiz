10c800aec0862cbe2122612c819b7e197188881c
Eduardo Rodrigues Oliveira <159492869+eduardohro@users.noreply.github.com>
2025-03-02 15:09:41 -0300

    Add questions

---
+++ questions.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+        numb: 1,
+        question: "O que significa HTML?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+
+        ]
+    },
+    {
+        numb: 2,
+        question: "O que significa CSS?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Codehal Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "O que significa PHP?",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Hometext Programming",
+            "C. Hypertext Preprogramming",
+            "D. Programming Hypertext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "O que significa SQL?",
+        answer: "D. Structured Query Language",
+        options: [
+            "A. Strength Query Language",
+            "B. Stylesheet Query Language",
+            "C. Science Question Language",
+            "D. Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "O que significa SQL?",
+        answer: "B. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Extensible Markup Language",
+            "C. Extra Markup Language",
+            "D. Explore Multiple Language"
+        ]
+    }
+];
