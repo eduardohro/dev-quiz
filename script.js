d041ae3f38fdb0f364b0664b7f7f21f30d803ff3
Eduardo Rodrigues Oliveira <159492869+eduardohro@users.noreply.github.com>
2025-03-02 15:42:03 -0300

    Add script questions and button next script

--- script.js
+++ script.js
@@ -21,4 +21,45 @@ continueBtn.onclick = () => {
     popupInfo.classList.remove('active');
     main.classList.remove('active');
     quizBox.classList.remove('active');
+
+    showQuestions(0);
+    questionCounter(1);
+}
+
+let questionCount = 0;
+let questionNumb = 1;
+
+const nextBtn = document.querySelector('.next-btn')
+
+nextBtn.onclick = () => {
+    if (questionCount < questions.length - 1) {
+        questionCount++;
+        showQuestions(questionCount);
+
+        questionNumb++;
+        questionCounter(questionNumb);
+
+    } else {
+        console.log('Pergunta concluída')
+    }
+}
+
+const optionList = document.querySelector('.option-list')
+
+// obtendo perguntas e opções de array
+function showQuestions(index) {
+    const questionText = document.querySelector('.question-text');
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+        <div class="option"><span>${questions[index].options[1]}</span></div>
+        <div class="option"><span>${questions[index].options[2]}</span></div>
+        <div class="option"><span>${questions[index].options[3]}</span></div>`;
+
+    optionList.innerHTML = optionTag;
+}
+
+function questionCounter(index) {
+    const questionTotal = document.querySelector('.question-total');
+    questionTotal.textContent = `${index} of ${questions.length} Questions`;
 }
